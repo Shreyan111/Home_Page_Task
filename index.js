91180c7b310a0548b6aec02feadade80371d3aae
Shreyan111 <shreyan.sanyal2020@vitstudent.ac.in>
2021-11-12 16:11:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+// This part helps in adding animations to the Text.
+
+consoleText(['Hello Freshers!!', 'This Portal', 'Allow Companies', 'To Recruit', 'Managers &' , 'Developers!!'], 'text',['white','white','white', 'white','white','white']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 8000)
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("sticky");
+      document.getElementById("navbar").style = 'background-color: #220936; height:160px';
+    } else {
+      navbar.classList.remove("sticky");
+      document.getElementById("navbar").style = 'background-color: #2c054d';
+    }
+  } else {
+    mybutton.style.display = "none";
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("sticky");
+      document.getElementById("navbar").style = 'background-color: #220936; height:160px';
+    } else {
+      navbar.classList.remove("sticky");
+      document.getElementById("navbar").style = 'background-color: #2c054d';
+    }
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
